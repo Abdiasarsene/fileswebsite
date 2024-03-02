c8d91b1042f21d00c26b26f04a994c7621165741
Abdiasarsene <abdiasarsene@gmail.com>
2024-03-01 18:58:42 -1100

    Analyses des donnees

--- ttest.py
+++ ttest.py
@@ -1,1 +1,11 @@
-print("hello")
+import pandas as pd
+db= pd.read_excel(r'c:\Users\ARMIDE Informatique\Desktop\Projet Académique\Projet personnel\Mémoire Abdias-Rodrigue\Base de données mémoire RETENUE.xlsx')
+
+print(db)
+#Statistique de toutes kes variables
+statdb = db.drop(columns=['Annees']).describe()
+print(statdb)
+
+#Deleting one variable
+led =db.pop('Recette gloable')
+print(led)
