844ad029b1c1e3646e7870f42e6d4aec55a32523
Abdiasarsene <abdiasarsene@gmail.com>
2024-03-01 20:59:51 -1100

    Création de formulaire

---
+++ digit.js
@@ -0,0 +1,6 @@
+let paragraphe = 'Je vais maîtriser JavaScript'
+let pgrph = document.createElement("p")
+pgrph.innerContent =paragraphe
+
+let corps = document.querySelector("body")
+corps = appendChild(pgrph)
