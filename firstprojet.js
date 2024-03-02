263a844388b6eb0b77c909a3e4cdff1f93c930dc
Abdiasarsene <abdiasarsene@gmail.com>
2024-03-01 16:00:54 -1100

    "#"

--- firstprojet.js
+++ firstprojet.js
@@ -25,3 +25,28 @@ let acceslink = document.getElementById("bouton");
 acceslink.addEventListener("click", function(){
 console.log('Le bouton vient dêtre cliqué')
 })
+
+//Ajouter un gestionnaire d'événement
+let rdv = document.getElementsByClassName("rdv");
+rdv.addEventListener("click", function handClick (event){
+    console.log("Je donne accès ç tout"+event.type)
+})
+
+//Ajouter un événement 
+let devis= document.getElementsByClassName(devis);
+devis.addEventListener(keypress, function(){
+    console.log("Damn bro, you got do all")
+})
+
+//Création d'une nouvelle section
+let sec5 = document.createElement("sec5");
+let H1 = document.createElement("h1")
+//Ajouter une classe à H1
+H1.classList.add(bloc6)
+//ajouter un titre à H1
+H1.textContent = 'Nous sommes disponibles'
+//Ajouter H1 à la section sec5
+sec5.appendChild(H1)
+
+let body = querySelector(body)
+body.appendChild(sec5)
