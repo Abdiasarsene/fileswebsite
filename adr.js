71c89e848de0a7e8e51055827b524443996fdeef
Abdiasarsene <abdiasarsene@gmail.com>
2024-03-06 06:04:02 -1100

    JavaScript

--- adr.js
+++ adr.js
@@ -1,2 +1,7 @@
-console.log("Hello World")
-alert('ok')
+let nompara = 'Bonsoir les amis';
+let titre1 = document.getElementsByClassName('titre1')[0]; // Ajout de [0] pour sélectionner le premier élément avec la classe 'titre1'
+
+titre1.innerHTML = nompara; // Utilisation de l'opérateur '=' pour assigner la valeur de nompara à innerHTML
+
+let body = document.querySelector('body'); // Correction de la syntaxe de querySelector
+body.prependChild(titre1); // Ajout de titre1 en tant qu'enfant de body
